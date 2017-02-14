30a883b09f0f1068c02aa92aece9aea4e3fc9f96
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2017-02-14 16:49:29 +0200

    add project

---
+++ app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, NgZone } from "@angular/core";
+import {session, Session, Task} from "nativescript-background-http";
+import {knownFolders } from "file-system";
+import {Page} from "ui/page";
+import {Image} from "ui/image";
+class tmpTask{
+    upload: number;
+
+    totalUpload: number;
+}
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html",
+})
+export class AppComponent { 
+    public newsession:Session;
+    public fileURL:string="";
+    public request:any;
+    public imageName:string="bigpic";
+    private documents;
+    public task:Task;
+
+    constructor(private page:Page, private zone:NgZone){
+         this.documents = knownFolders.currentApp();
+         this.fileURL = this.documents.path+"/bigpic.jpg";
+         this.newsession = session("image-upload");
+         this.request = {
+            url: "http://httpbin.org/post",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/octet-stream",
+                "File-Name": this.imageName
+            },
+            description: "{ 'uploading': " + this.imageName + " }"
+        };
+    }
+    onTap2(){
+        var params = [{name: "test", value: "value"}, {name:"fileToUpload", filename: this.fileURL, mimeType: 'image/jpeg'}];
+       this.task = this.newsession.multipartUpload(params, this.request);
+       this.task.on("progress", this.logEvent);
+       this.task.on("error", this.logEvent);
+       this.task.on("complete", this.logEvent);
+    }
+    onTap(){
+       this.task = this.newsession.uploadFile(this.fileURL, this.request);
+       this.task.on("progress", this.logEvent);
+       this.task.on("error", this.logEvent);
+       this.task.on("complete", this.logEvent);
+    }
+    logEvent(e) {
+        console.log("currentBytes: " + e.currentBytes);
+        console.log("totalBytes: " + e.totalBytes);
+        console.log("eventName: " + e.eventName);
+        
+    }
+}
---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { AppRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
+
+import { ItemService } from "./item/item.service";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
