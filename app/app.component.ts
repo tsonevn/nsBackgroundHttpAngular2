d45b86c49d88fc05ca0abcddee5cadac615cfe98
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2017-02-21 13:56:36 +0200

    update project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, NgZone } from "@angular/core";
+import { Component, NgZone, ViewChild, ElementRef } from "@angular/core";
 import {session, Session, Task} from "nativescript-background-http";
 import {knownFolders } from "file-system";
 import {Page} from "ui/page";
 import {Image} from "ui/image";
-import {Progress} from "ui/progress"
+import {Progress} from "ui/progress";
+import {fromFile, ImageSource} from "image-source"
+
 class tmpTask{
     upload: number;
 
@@ -18,14 +20,16 @@ export class AppComponent {
     public newsession:Session;
     public fileURL:string="";
     public request:any;
-    public imageName:string="bigpic";
+    public imageName:string="logo";
     private documents;
     public task:Task;
-    public value = 0;
+    public imageSrc ="~/logo.png";
+
+    @ViewChild("myImage") myImageRef: ElementRef;
 
     constructor(private page:Page, private zone:NgZone){
          this.documents = knownFolders.currentApp();
-         this.fileURL = this.documents.path+"/bigpic.jpg";
+         this.fileURL = this.documents.path+"/logo.png";
          this.newsession = session("image-upload");
          this.request = {
             url: "http://httpbin.org/post",
@@ -37,18 +41,22 @@ export class AppComponent {
             description: "{ 'uploading': " + this.imageName + " }"
         };
     }
-    onTap2(){
-        var params = [{name: "test", value: "value"}, {name:"fileToUpload", filename: this.fileURL, mimeType: 'image/jpeg'}];
+    multiUpload(){
+        var progress:Progress =<Progress> this.page.getViewById("prgressmultiid");
+            progress.value = 0;
+        var params = [{name: "test", value: "value"}, {name:"fileToUpload", filename: this.fileURL, mimeType: 'image/png'}];
        this.task = this.newsession.multipartUpload(params, this.request);
        this.task.on("progress", (e)=>{
-            var progress:Progress =<Progress> this.page.getViewById("prgressid");
+            var progress:Progress =<Progress> this.page.getViewById("prgressmultiid");
             progress.value = e.currentBytes;
             progress.maxValue = e.totalBytes;
        });
        this.task.on("error", this.logEvent);
        this.task.on("complete", this.logEvent);
     }
-    onTap(){
+    imageUplaod(){
+        var progress:Progress =<Progress> this.page.getViewById("prgressid");
+            progress.value = 0;
        this.task = this.newsession.uploadFile(this.fileURL, this.request);
        this.task.on("progress", (e)=>{
             var progress:Progress =<Progress> this.page.getViewById("prgressid");
@@ -60,12 +68,19 @@ export class AppComponent {
        
     }
     logEvent(e) {
+        switch (e.eventName) {
+            case "complete":
+                alert("Upload complete");
+                break;
+            case "error":
+                alert("Upload error"+e)
+                break;
         
-        console.log("currentBytes: " + e.currentBytes);
-        if((e.currentBytes != null) && (e.currentBytes !=undefined)){
-
-            
+            default:
+                break;
         }
+
+        
        
         
     }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
-import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
 
 @NgModule({
     bootstrap: [
